b58acfec6a278630b0182776343c0b68729eb004
João Gabriel Trautmann <155569420+JoaoGST@users.noreply.github.com>
2026-03-17 15:01:04 -0300

    Add scroll effect to navigation and clean up code

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ function typeEffect() {
 }
 
 typeEffect();
+
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
